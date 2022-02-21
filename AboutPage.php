550760427cedfc8ca6a164c2f4f101a72bf38212
John Duc-Tuan Nguyen <nguyenjd@lgtd-lt-75-mbmt.stanford.edu>
2022-02-21 14:33:19 -0800

    test change to about page

--- AboutPage.php
+++ AboutPage.php
@@ -10,7 +10,7 @@
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/cover/">
 
-    
+
 
     <!-- Bootstrap core CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -32,12 +32,12 @@
       }
     </style>
 
-    
+
     <!-- Custom styles for this template -->
     <link href="AboutPage.css" rel="stylesheet">
   </head>
   <body class="d-flex h-100 text-center text-white bg-dark">
-    
+
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
@@ -54,7 +54,7 @@
 
   <main class="px-3">
     <h1>About Us</h1>
-    <p class="lead">We are a group of Stanford Undergraduates who are passionate about sharing stories with others. With this project we aim to create a tool that helps social media users enhance the quality of their media posts. We help them by creating a one-of-a-kind capsule of their shared photo experiences.</p>
+    <p class="lead">Hi! :) We are a group of Stanford Undergraduates who are passionate about sharing stories with others. With this project we aim to create a tool that helps social media users enhance the quality of their media posts. We help them by creating a one-of-a-kind capsule of their shared photo experiences.</p>
     <p class="lead">
     </p>
   </main>
@@ -65,7 +65,6 @@
 </div>
 
 
-    
+
   </body>
 </html>
-
