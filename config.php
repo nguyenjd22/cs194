132b033ad2121f23e4845cf0aec03e34624d9345
Mai Lan Nguyen <nmailan@stanford.edu>
2022-02-21 17:34:05 -0800

    config change 5

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'bitnami');
-define('DB_PASSWORD', 'password194');
-define('DB_NAME', 'Usersinfo');
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'bitnami');
+// define('DB_PASSWORD', 'password194');
+// define('DB_NAME', 'Usersinfo');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect('3.130.151.29', 'bitnami', 'password194', 'User_Data');
+$link = mysqli_connect('localhost', 'bitnami', 'password194', 'User_Data');
  
 // Check connection
 if($link === false){
