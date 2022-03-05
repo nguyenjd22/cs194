b6fee3fd7dcb737e4b83866e59d2f4f70f15856a
Mai Lan Nguyen <nmailan@stanford.edu>
2022-03-05 14:28:37 -0800

    saving

--- collage.php
+++ collage.php
@@ -7,6 +7,111 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
+// require_once "config.php";
+// // Processing form data when form is submitted
+// if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+//   // Validate first name
+//   if(empty(trim($_POST["first_name"]))){
+//     $first_name_err = "Please enter your first name.";     
+//   } else{
+//       $first_name = trim($_POST["first_name"]);
+//   }
+
+//   // Validate last name
+//   if(empty(trim($_POST["last_name"]))){
+//     $first_name_err = "Please enter your last name.";     
+//   } else{
+//       $last_name = trim($_POST["last_name"]);
+//   }
+
+//   // Validate username
+//   if(empty(trim($_POST["username"]))){
+//       $username_err = "Please enter a username.";
+//   } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+//       $username_err = "Username can only contain letters, numbers, and underscores.";
+//   } else{
+//       // Prepare a select statement
+//       $sql = "SELECT id FROM users WHERE username = ?";
+      
+//       if($stmt = mysqli_prepare($link, $sql)){
+//           // Bind variables to the prepared statement as parameters
+//           mysqli_stmt_bind_param($stmt, "s", $param_username);
+          
+//           // Set parameters
+//           $param_username = trim($_POST["username"]);
+          
+//           // Attempt to execute the prepared statement
+//           if(mysqli_stmt_execute($stmt)){
+//               /* store result */
+//               mysqli_stmt_store_result($stmt);
+              
+//               if(mysqli_stmt_num_rows($stmt) == 1){
+//                   $username_err = "This username is already taken.";
+//               } else{
+//                   $username = trim($_POST["username"]);
+//               }
+//           } else{
+//               echo "Oops! Something went wrong. Please try again later. Validate Username.";
+//           }
+
+//           // Close statement
+//           mysqli_stmt_close($stmt);
+//       }
+//   }
+  
+//   // Validate password
+//   if(empty(trim($_POST["password"]))){
+//       $password_err = "Please enter a password.";     
+//   } elseif(strlen(trim($_POST["password"])) < 6){
+//       $password_err = "Password must have atleast 6 characters.";
+//   } else{
+//       $password = trim($_POST["password"]);
+//   }
+  
+//   // Validate confirm password
+//   if(empty(trim($_POST["confirm_password"]))){
+//       $confirm_password_err = "Please confirm password.";     
+//   } else{
+//       $confirm_password = trim($_POST["confirm_password"]);
+//       if(empty($password_err) && ($password != $confirm_password)){
+//           $confirm_password_err = "Password did not match.";
+//       }
+//   }
+  
+//   // Check input errors before inserting in database
+//   if(empty($first_name_err) && empty($last_name_err) && empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+      
+//       // Prepare an insert statement
+//       $sql = "INSERT INTO users (first_name, last_name, username, password) VALUES (?, ?, ?, ?)";
+       
+//       if($stmt = mysqli_prepare($link, $sql)){
+//           // Bind variables to the prepared statement as parameters
+//           mysqli_stmt_bind_param($stmt, "ssss", $param_first_name, $param_last_name, $param_username, $param_password);
+          
+//           // Set parameters
+//           $param_first_name = $first_name;
+//           $param_last_name = $last_name;
+//           $param_username = $username;
+//           $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+          
+//           // Attempt to execute the prepared statement
+//           if(mysqli_stmt_execute($stmt)){
+//               // Redirect to login page
+//               header("location: login.php");
+//           } else{
+//               echo "Oops! Something went wrong. Please try again later. SQL query.";
+//           }
+
+//           // Close statement
+//           mysqli_stmt_close($stmt);
+//       }
+//   }
+  
+//   // Close connection
+//   mysqli_close($link);
+// }
+
 ?>
 <html lang="en" class="h-100">
   <head>
@@ -79,8 +184,33 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       </main>
       <div id="cnvdiv"></div>
       <a id="download" download="collage.png">
-        <button id ="svbtn" hidden="hidden" onclick="saveImg()" class="btn btn-lg btn-secondary fw-bold border-white big-white">Save Collage</button>
+        <button id ="svbtn" hidden="hidden" onclick="saveImg()" class="btn btn-lg btn-secondary fw-bold border-white big-white">Download Collage</button>
       </a>
+      <!-- <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <input type="text" placeholder="First Name" name="first_name" class="form-control <?php echo (!empty($first_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $first_name; ?>">
+                <span class="invalid-feedback"><?php echo $first_name_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <input type="text" placeholder="Last Name" name="last_name" class="form-control <?php echo (!empty($last_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $last_name; ?>">
+                <span class="invalid-feedback"><?php echo $last_name_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <input type="text" placeholder="Username" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <input type="password" placeholder="Password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Confirm Password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-lg btn-secondary fw-bold border-white big-white" value="Save to profile">
+            </div>
+        </form> -->
       <footer class="mt-auto text-white-50">
         <p>Stanford CS194 Project Round-About</p>
       </footer>
@@ -309,6 +439,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         var img = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
         download.setAttribute("href", img);
         console.log(img);
+
+        var img_file = canvas.toDataURL("image/png");
+        saveAs(img_file, "collage.png");
       };
   </script>
 </html>
