3e0cbb9f54a3e0fa538464befd3758e6bf22ea87
John Duc-Tuan Nguyen <nguyenjd@lgtd-lt-75-mbmt.stanford.edu>
2022-03-05 14:41:59 -0800

    test saveAs

--- collage.php
+++ collage.php
@@ -10,17 +10,17 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 // require_once "config.php";
 // // Processing form data when form is submitted
 // if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+
 //   // Validate first name
 //   if(empty(trim($_POST["first_name"]))){
-//     $first_name_err = "Please enter your first name.";     
+//     $first_name_err = "Please enter your first name.";
 //   } else{
 //       $first_name = trim($_POST["first_name"]);
 //   }
 
 //   // Validate last name
 //   if(empty(trim($_POST["last_name"]))){
-//     $first_name_err = "Please enter your last name.";     
+//     $first_name_err = "Please enter your last name.";
 //   } else{
 //       $last_name = trim($_POST["last_name"]);
 //   }
@@ -33,19 +33,19 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 //   } else{
 //       // Prepare a select statement
 //       $sql = "SELECT id FROM users WHERE username = ?";
-      
+
 //       if($stmt = mysqli_prepare($link, $sql)){
 //           // Bind variables to the prepared statement as parameters
 //           mysqli_stmt_bind_param($stmt, "s", $param_username);
-          
+
 //           // Set parameters
 //           $param_username = trim($_POST["username"]);
-          
+
 //           // Attempt to execute the prepared statement
 //           if(mysqli_stmt_execute($stmt)){
 //               /* store result */
 //               mysqli_stmt_store_result($stmt);
-              
+
 //               if(mysqli_stmt_num_rows($stmt) == 1){
 //                   $username_err = "This username is already taken.";
 //               } else{
@@ -59,42 +59,42 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 //           mysqli_stmt_close($stmt);
 //       }
 //   }
-  
+
 //   // Validate password
 //   if(empty(trim($_POST["password"]))){
-//       $password_err = "Please enter a password.";     
+//       $password_err = "Please enter a password.";
 //   } elseif(strlen(trim($_POST["password"])) < 6){
 //       $password_err = "Password must have atleast 6 characters.";
 //   } else{
 //       $password = trim($_POST["password"]);
 //   }
-  
+
 //   // Validate confirm password
 //   if(empty(trim($_POST["confirm_password"]))){
-//       $confirm_password_err = "Please confirm password.";     
+//       $confirm_password_err = "Please confirm password.";
 //   } else{
 //       $confirm_password = trim($_POST["confirm_password"]);
 //       if(empty($password_err) && ($password != $confirm_password)){
 //           $confirm_password_err = "Password did not match.";
 //       }
 //   }
-  
+
 //   // Check input errors before inserting in database
 //   if(empty($first_name_err) && empty($last_name_err) && empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-      
+
 //       // Prepare an insert statement
 //       $sql = "INSERT INTO users (first_name, last_name, username, password) VALUES (?, ?, ?, ?)";
-       
+
 //       if($stmt = mysqli_prepare($link, $sql)){
 //           // Bind variables to the prepared statement as parameters
 //           mysqli_stmt_bind_param($stmt, "ssss", $param_first_name, $param_last_name, $param_username, $param_password);
-          
+
 //           // Set parameters
 //           $param_first_name = $first_name;
 //           $param_last_name = $last_name;
 //           $param_username = $username;
 //           $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-          
+
 //           // Attempt to execute the prepared statement
 //           if(mysqli_stmt_execute($stmt)){
 //               // Redirect to login page
@@ -107,7 +107,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 //           mysqli_stmt_close($stmt);
 //       }
 //   }
-  
+
 //   // Close connection
 //   mysqli_close($link);
 // }
@@ -190,15 +190,15 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             <div class="form-group">
                 <input type="text" placeholder="First Name" name="first_name" class="form-control <?php echo (!empty($first_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $first_name; ?>">
                 <span class="invalid-feedback"><?php echo $first_name_err; ?></span>
-            </div>    
+            </div>
             <div class="form-group">
                 <input type="text" placeholder="Last Name" name="last_name" class="form-control <?php echo (!empty($last_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $last_name; ?>">
                 <span class="invalid-feedback"><?php echo $last_name_err; ?></span>
-            </div>    
+            </div>
             <div class="form-group">
                 <input type="text" placeholder="Username" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
+            </div>
             <div class="form-group">
                 <input type="password" placeholder="Password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
@@ -441,27 +441,45 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         console.log(img);
 
         var img_file = canvas.toDataURL("image/png");
-        // saveAs(img_file, "collage.png");
+        saveAs(img_file, "collage.png");
 
-        fs.writeFile("./images/" + filename, img_file, function (err) {
-              //Once you have the file written into your images directory under the name
-              // filename you can create the Photo object in the database
-              if (err) {
-                console.error('Doing /photos/new', err);
-                response.status(400).send(JSON.stringify(err));
-                return;
-              }
-              function doneCallback(err, newPhoto) {
-                if (err) {
-                    console.error('Failed to create photo', err);
-                    response.status(400).send(JSON.stringify(err));
-                    return;
-                }
-                // newPhoto.save();
-                // response.status(200).send("Upload Successful.");
-                // console.log('Created Photo with ID', newPhoto._id);
-              }
-            });
+        function saveAs(uri, filename){
+            var link = document.createElement('a');
+
+            if (typeof link.download === 'string'){
+                link.href = uri;
+                link.download = filename;
+
+                document.body.appendChild(link);
+
+                link.click();
+
+                document.body.removeChild(link);
+            } else {
+              window.open(uri);
+            }
+
+        };
+
+        // fs.writeFile("./images/" + filename, img_file, function (err) {
+        //       //Once you have the file written into your images directory under the name
+        //       // filename you can create the Photo object in the database
+        //       if (err) {
+        //         console.error('Doing /photos/new', err);
+        //         response.status(400).send(JSON.stringify(err));
+        //         return;
+        //       }
+        //       function doneCallback(err, newPhoto) {
+        //         if (err) {
+        //             console.error('Failed to create photo', err);
+        //             response.status(400).send(JSON.stringify(err));
+        //             return;
+        //         }
+        //         // newPhoto.save();
+        //         // response.status(200).send("Upload Successful.");
+        //         // console.log('Created Photo with ID', newPhoto._id);
+        //       }
+        //     });
       };
   </script>
 </html>
