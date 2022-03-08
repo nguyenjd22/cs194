66e925c7797631005b6006843ed30de6c934c90b
John Duc-Tuan Nguyen <nguyenjd@lgtd-lt-75-mbmt.stanford.edu>
2022-03-08 10:45:33 -0800

    remove hardcoded header

--- homepage.php
+++ homepage.php
@@ -51,7 +51,7 @@
           <a style="text-decoration: none;" class="menuItem" href="instructions.php">Help</a>
         </td>
         <td width="20%">
-          <h3>Hello, ____</h3>
+          <!-- <h3>Hello, ____</h3> -->
           <?php
           session_start();
           if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
