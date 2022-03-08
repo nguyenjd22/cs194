adac19aaa82e0d398f035b12425f0aff2ca95a76
John Duc-Tuan Nguyen <nguyenjd@lgtd-lt-75-mbmt.stanford.edu>
2022-03-08 10:58:08 -0800

    adjust location of name

--- homepage.php
+++ homepage.php
@@ -50,7 +50,7 @@
           ?>
           <a style="text-decoration: none;" class="menuItem" href="instructions.php">Help</a>
         </td>
-        <td width="10%">
+        <td width="13%">
           <?php
           session_start();
           if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
