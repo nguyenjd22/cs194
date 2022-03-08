bb5b38e79b25f85ce7a618bcfd441d8cc24bd6e5
Mai Lan Nguyen <nmailan@stanford.edu>
2022-03-07 23:54:57 -0800

    homepage

--- AboutPage.php
+++ AboutPage.php
@@ -43,14 +43,14 @@
       <table class="toolBar">
         <tr>
           <td class="menuItems">
-            <a style="text-decoration: none;" href="homepage.html" class="menuItem">Home</a>
+            <a style="text-decoration: none;" href="homepage.php" class="menuItem">Home</a>
             <a style="text-decoration: none;" class="menuItem active" aria-current="page" href="#">About</a>
             <!-- GATING SYSTEM - AUTHORIZATION PAGE IF NOT AUTHORIZED; OTHERWISE - DATEPICKER -->
             <a style="text-decoration: none;" class="menuItem" href="datepicker.php">Create</a>
           </td>
           <td width="10%">
             <button class="logoutButton">
-              <a class="logoutText" href="logout.php" margin-right="0px">Logout</a>
+              <a style="text-decoration: none;" class="logoutText" href="logout.php" margin-right="0px">Logout</a>
             </button>
           </td>
         </tr>
