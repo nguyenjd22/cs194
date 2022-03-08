bb47486cb902750d84c77a2c8b3656f81424bf88
John Duc-Tuan Nguyen <nguyenjd@lgtd-lt-75-mbmt.stanford.edu>
2022-03-08 02:10:25 -0800

    ui fix 4

--- countryPicker.php
+++ countryPicker.php
@@ -330,8 +330,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   /* background-color: #5e5e5e; */
   background-color: white;
   height: 45px;
-  padding-top: 20px;
-  padding-left: 20px;
+  padding-top: 15px;
+  padding-left: 15px;
+  font-size:16px;
 }
 .menuItems {
  padding-left:40px;
