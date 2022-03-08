4f9a8312e673a4d30125378b4047ea09b8f399da
John Duc-Tuan Nguyen <nguyenjd@lgtd-lt-75-mbmt.stanford.edu>
2022-03-08 13:50:47 -0800

    change title of datepicker

--- datepicker.php
+++ datepicker.php
@@ -33,7 +33,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <link rel="stylesheet" href="datepicker_style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Generate Collage</title>
+    <title>Create</title>
   </head>
   <body>
     <table class="toolBar">
