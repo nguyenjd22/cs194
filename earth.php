6253cdbeb703b7b1522cac17bbe0d613a6851e13
CarolineXZ <93568704+CarolineXZ@users.noreply.github.com>
2022-03-07 23:31:03 -0800

    nav to homepage.html when tab home

--- earth.php
+++ earth.php
@@ -339,7 +339,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <a class="nav-link active" aria-current="page" href="#">Map</a>
         <a class="nav-link" href="AboutPage.php">About</a>
-        <a class="nav-link" href="datepicker.php">Home</a>
+        <a class="nav-link" href="homepage.html">Home</a>
       </nav>
     </div>
   </header>
