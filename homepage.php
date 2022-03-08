0fea2cd0b3521d064017f32f5b6b82ce3230b276
John Duc-Tuan Nguyen <nguyenjd@lgtd-lt-75-mbmt.stanford.edu>
2022-03-08 00:59:29 -0800

    homepage gateway logic

--- homepage.php
+++ homepage.php
@@ -109,15 +109,18 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             session_start();
             echo '<h1>Hello, '.$_SESSION ['first_name'].'</h1>';
             if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-              echo 'hi';
+              echo '<p>Welcome to Round-About, create a collage or interactive map that encapsulates your travel experiences.</p>';
+              echo '<p><a class="btn btn-lg btn-primary" href="login.php">Sign up/Login</a></p>';
             } else {
               echo '<p>Welcome to Round-About, create a collage or interactive map that encapsulates your travel experiences.</p>';
+              echo '<p><a class="btn btn-lg btn-primary" href="datepicker.php">Create</a></p>'
             }
             ?>
             <!-- <h1>Hello (NAME)</h1> -->
-            <p>Welcome to Round-About, create a collage or interactive map that encapsulates your travel experiences.</p>
+            <!-- <p>Welcome to Round-About, create a collage or interactive map that encapsulates your travel experiences.</p> -->
 
-            <p><a class="btn btn-lg btn-primary" href="datepicker.php">Create</a></p>
+            <!-- <p><a class="btn btn-lg btn-primary" href="datepicker.php">Create</a></p>
+            <p><a class="btn btn-lg btn-primary" href="login.php">Sign up/Login</a></p> -->
           </div>
         </div>
       </div>
