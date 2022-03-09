1faa0d2aa05f4abaad325a45ce69450012e66d9d
Mai Lan Nguyen <nmailan@stanford.edu>
2022-03-08 22:35:26 -0800

    main text change

--- AboutPage.php
+++ AboutPage.php
@@ -90,7 +90,7 @@
         </div>
       </header>
       <main class="px-3 mx-10" id="authorizeBottom">
-        <h1>About Us</h1>
+        <h1 class="textHeader">About Us</h1>
         <p class="lead">We are a group of Stanford Undergraduates who are passionate about sharing stories with others. With this project we aim to create a tool that helps social media users enhance the quality of their media posts. We help them by creating a one-of-a-kind capsule of their shared photo experiences.</p>
       </main>
       <footer class="mt-auto text-white-50">
