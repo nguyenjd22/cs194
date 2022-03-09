daa89b1f1ac20006a7933ab9ea6c0227e8eb62bb
CarolineXZ <93568704+CarolineXZ@users.noreply.github.com>
2022-03-08 23:50:56 -0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -28,6 +28,9 @@
           font-size: 3.5rem;
         }
       }
+      .intro-text{
+        margin-left: 30px;
+      }
     </style>
 
 
@@ -202,12 +205,11 @@
     <hr class="featurette-divider">
 
     <div class="row featurette">
-      <div class="col-md-7 order-md-2">
+      <div class="col-md-7 order-md-2 intro-text">
         <h2 class="featurette-heading">Automatically Generate Travel Collages</h2>
         <p class="lead">Click through our different collage options, decide which one you like best and share with your friends.</p>
       </div>
-      <div class="col-md-5">
-        <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg> -->
+      <div class="col-md-5 intro-image">
         <img src="collage-ad.png" width="500" height="500"/>
       </div>
     </div>
