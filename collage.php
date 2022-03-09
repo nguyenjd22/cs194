e6a336024081e22d3f79a0486abf48533e54f756
Mai Lan Nguyen <nmailan@stanford.edu>
2022-03-08 21:53:08 -0800

    align buttons 2

--- collage.php
+++ collage.php
@@ -163,10 +163,16 @@ require_once "config.php";
             <td width="50%">
               <table>
                 <tr class="buttonRow">
+                  <td>
                   <a id="backgrounds">
                     <button id ="mountain_button" class="btn btn-lg btn-primary">Choose Mountain Background</button>
-                    <button id ="beach_button" class="btn btn-lg btn-primary">Choose Beach Background</button>
                   </a>
+                  </td>
+                  <td>
+                    <a id="backgrounds">
+                      <button id ="beach_button" class="btn btn-lg btn-primary">Choose Beach Background</button>
+                    </a>
+                  </td>
                 </tr>
                 <tr class="buttonRow">
                   <a id="draws">
