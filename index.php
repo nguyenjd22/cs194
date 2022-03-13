1d575556a7bee8112c8f0853fb83c0ca947b6c9a
CarolineXZ <93568704+CarolineXZ@users.noreply.github.com>
2022-03-13 16:10:59 -0700

    cleanup

--- index.php
+++ index.php
@@ -47,8 +47,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       <table class="toolBar">
       <tr>
         <td class="menuItems">
-          <!-- <a style="text-decoration: none;" href="homepage.php" class="menuItem">Home</a> -->
-          <!-- <a  style="text-decoration: none;" class="menuItem" href="AboutPage.php">About</a> -->
         </td>
         <td width="13%">
           <?php
