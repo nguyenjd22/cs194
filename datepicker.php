9c27c1e5833f9b2dedca30034ef373773f30e3f2
Mai Lan Nguyen <nmailan@stanford.edu>
2022-03-14 09:42:13 +0700

    rename and relink datepicker style file

--- datepicker.php
+++ datepicker.php
@@ -30,7 +30,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <!-- Style -->
-    <link rel="stylesheet" href="datepicker_style.css">
+    <link rel="stylesheet" href="datepicker.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Create</title>
