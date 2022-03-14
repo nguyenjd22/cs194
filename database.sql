a9770a5bbe07f94f5dbc041f1bfa62ef036e0de1
bmberger <brianaberger@DNa8089c5.SUNet>
2022-03-14 10:11:24 -0700

    Formatted database.sql

--- database.sql
+++ database.sql
@@ -13,25 +13,31 @@ CREATE TABLE users (
 
 desc users;
 
-INSERT INTO users (first_name, last_name, username, password) VALUES
-("John", "Nguyen", "njohn", "john1"),
-("Maia", "Nguyen", "nmaia", "maia1")
-
-SELECT * FROM users;
-
+INSERT INTO
+    users (first_name, last_name, username, password)
+VALUES
+    ("John", "Nguyen", "njohn", "john1"),
+    ("Maia", "Nguyen", "nmaia", "maia1")
+SELECT
+    *
+FROM
+    users;
 
 CREATE TABLE photos (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     file_name VARCHAR(50) NOT NULL,
-    photo_type VARCHAR(50) NOT NULL
-    -- image longblob NOT NULL
+    photo_type VARCHAR(50) NOT NULL -- image longblob NOT NULL
 );
 
 desc photos;
 
-INSERT INTO photos (user_id, file_name, photo_type) VALUES
-(8, "good_job.png", "background"),
-(8, "good_job.png", "collage")
-
-SELECT * FROM photos;
+INSERT INTO
+    photos (user_id, file_name, photo_type)
+VALUES
+    (8, "good_job.png", "background"),
+    (8, "good_job.png", "collage")
+SELECT
+    *
+FROM
+    photos;
