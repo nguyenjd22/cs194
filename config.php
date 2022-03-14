4616b4472cbd881d2db2c112a858595e6267a725
Mai Lan Nguyen <nmailan@stanford.edu>
2022-03-14 13:07:33 +0700

    cleanup config.php code

--- config.php
+++ config.php
@@ -1,11 +1,4 @@
-<?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'bitnami');
-// define('DB_PASSWORD', 'password194');
-// define('DB_NAME', 'Usersinfo');
- 
+<?php 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect('ls-336b81bab071ebb8e843329caf50c3e20df54d82.cr2wibycqrus.us-east-2.rds.amazonaws.com', 'dbmasteruser', 'password194', 'User_Data');
  
